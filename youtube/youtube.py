4027807c162d741e6f3631eea8e772f79b1f1245
Joel Ackam <ackamjoel@gmail.com>
2023-12-29 21:04:49 +0000

    youtube video downloader

--- youtube/youtube.py
+++ youtube/youtube.py
@@ -1,19 +1,38 @@
 from pytube import YouTube
 import tkinter as tk
-from tkinter import dialog
+from tkinter import filedialog
 
-
-def download_video(url, save_path):
+def download_video(url,save_path):
     try:
         yt = YouTube(url)
         streams = yt.streams.filter(progressive=True, file_extension='mp4')
-        highest_resolution = streams.get_highest_resolution()
-        highest_resolution.download(output_path=save_path)
-        print('Video downloaded Successfully!')
-
+        highest_quality = streams.get_highest_resolution()
+        highest_quality.download(output_path=save_path)
+        print('video downloaded successfully!')
+    
     except Exception as e:
         print(e)
 
-url = 'https://youtube.com/shorts/oXO3q2Kvc4U?si=JyUttUVR5TIVEZYj'
-save_path = 'C:/Users/ackam/Desktop'
-download_video(url, save_path)
+def open_file_dialog():
+    folder = filedialog.askdirectory()
+
+    if folder:
+        print(f'selcted folder: {folder}')
+
+    return folder
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.withdraw()
+
+    video_url = input('Kindly enter the url for the video: ')
+    save_path = open_file_dialog()
+
+    if save_path:      
+        print('download started...')
+        download_video(video_url,save_path)
+    
+    else:
+        print('Invalid DIrectory!')
+
+    
